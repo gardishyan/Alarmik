c7ae182caea491c25cdfc9bf344bcb4dc71af3f4
Henrik Gardishyan <henrik.gardishyan@gmail.com>
2023-04-27 19:34:58 +0400

    Mutable flag added

--- app/src/main/java/com/example/alarmik/NotificationHelper.java
+++ app/src/main/java/com/example/alarmik/NotificationHelper.java
@@ -16,9 +16,9 @@ import androidx.core.app.NotificationManagerCompat;
 import java.util.Random;
 
 public class NotificationHelper {
-
+    final static String CHANNEL_ID = "alarmik_channel_id";
     public static void createChannel(Context context) {
-        NotificationChannel channel = new NotificationChannel("alarmik_channel_id", "Alarmik Channel", NotificationManager.IMPORTANCE_HIGH);
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Alarmik Channel", NotificationManager.IMPORTANCE_HIGH);
         channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC); // to display on locked screen
 
         NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
@@ -31,9 +31,9 @@ public class NotificationHelper {
             //Any activity here
             Intent intent = new Intent(context, MainActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 100, intent, PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
 
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "my_channel_id")
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
                     .setSmallIcon(R.drawable.pomidor)
                     .setContentTitle(title)
                     .setContentText(content)
